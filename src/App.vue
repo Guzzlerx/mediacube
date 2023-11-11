8e911ee334a64185a3f8bbb3f58f2b0a5fdc8ac5
andrew <pasynkov2010@icloud.com>
2023-11-11 23:37:52 +0400

    init commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import TodoBlock from '@/components/TodoBlock.vue';
+
+export default {
+  components: { TodoBlock }
+};
+</script>
+
+<template>
+  <main class="app">
+    <TodoBlock />
+  </main>
+</template>
+
+<style scoped>
+.app {
+  max-width: 1440px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 90px 0;
+  background: linear-gradient(
+    to top left,
+    rgba(215, 190, 252, 1),
+    rgba(188, 177, 252, 1),
+    rgba(161, 174, 252, 1),
+    rgba(148, 189, 252, 1)
+  );
+}
+</style>
